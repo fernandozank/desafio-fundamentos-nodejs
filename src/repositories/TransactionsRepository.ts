bff781769d1700c145d167833a162e83ccf528b8
Fernando Zank <zank@pazinatoti.com.br>
2020-04-14 10:16:48 -0300

    Challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
+import { json } from 'express';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc: Balance, valorAtual: Transaction) => {
+        if (valorAtual.type === 'income') {
+          acc.income += valorAtual.value;
+        } else {
+          acc.outcome += valorAtual.value;
+        }
+
+        acc.total = acc.income - acc.outcome;
+
+        return acc;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const balance = this.getBalance();
+
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'outcome' && balance.total - value < 0) {
+      throw Error('x');
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
